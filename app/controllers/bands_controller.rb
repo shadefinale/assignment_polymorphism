6eb8bfabfdc8eb81ddc9d7ad10699fbe7349ac0a
shadefinale <shadefinale@gmail.com>
2015-07-30 17:32:08 -0500

    Can view index of either stadium or restaurant.

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,2 +1,6 @@
 class BandsController < ApplicationController
+  def index
+    id_type = params[:location].downcase + "_id"
+    @bands = params[:location].singularize.classify.constantize.find(params[id_type]).bands
+  end
 end
