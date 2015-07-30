e85d5c9ad7a6d27f23447b80057e507102c54253
shadefinale <shadefinale@gmail.com>
2015-07-30 17:51:17 -0500

    Can properly view bands of stadia or restaurants.

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,6 +1,11 @@
 class BandsController < ApplicationController
   def index
-    id_type = params[:location].downcase + "_id"
-    @bands = params[:location].singularize.classify.constantize.find(params[id_type]).bands
+    @bands = bands_by_location_type(params[:location])
   end
+
+  private
+    def bands_by_location_type(location)
+      id_type = location.singularize.downcase + "_id"
+      location.singularize.classify.constantize.find(params[id_type]).bands
+    end
 end
